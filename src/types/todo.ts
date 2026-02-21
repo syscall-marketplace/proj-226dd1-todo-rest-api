cbbcc4864d069f3eeaf3d8871aa6238f253aa845
Syscall Orchestrator <orchestrator@syscall.dev>
2026-02-21 17:01:19 -0500

    Scaffold: todo-rest-api

---
+++ src/types/todo.ts
@@ -0,0 +1,21 @@
+export interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+export interface CreateTodoRequest {
+  title: string;
+}
+
+export interface UpdateTodoRequest {
+  title?: string;
+  completed?: boolean;
+}
+
+export interface ApiResponse<T = any> {
+  success: boolean;
+  data?: T;
+  error?: string;
+}
